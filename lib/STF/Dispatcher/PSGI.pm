a99f2289dc103402e899c0f6132a005e4c376f91
lestrrat <lestrrat+github@gmail.com>
2011-11-28 15:04:20 +0900

    Initial commit

---
+++ lib/STF/Dispatcher/PSGI.pm
@@ -0,0 +1,523 @@
+package STF::Dispatcher::PSGI;
+use strict;
+our $VERSION = '1.00';
+use Carp ();
+use HTTP::Date ();
+use Plack::Request;
+use Plack::Middleware::HTTPExceptions;
+use Scalar::Util ();
+use Class::Accessor::Lite
+    rw => [ qw(impl) ]
+;
+use constant +{
+    STF_DEBUG                         => $ENV{STF_DEBUG},
+    STF_REPLICATION_HEADER            => 'X-STF-Replication-Count',
+    STF_REPLICATION_HEADER_DEPRECATED => 'X-Replication-Count',
+    STF_RECURSIVE_DELETE_HEADER       => 'X-STF-Recursive-Delete',
+    STF_CONSISTENCY_HEADER            => "X-STF-Consistency",
+    STF_FORCE_MASTER_HEADER           => "X-STF-Force-MasterDB",
+    STF_DELETED_OBJECTS_HEADER        => "X-STF-Deleted-Objects",
+    STF_DEFAULT_REPLICATION_COUNT     => 2,
+};
+
+sub new {
+    my ($class, %args) = @_;
+
+    my $impl = $args{impl} or
+        Carp::croak( "Required parameter 'impl' not specified" );
+    foreach my $method (qw(create_bucket get_bucket delete_bucket create_object get_object delete_object delete_bucket modify_object is_valid_object)) {
+        $impl->can( $method ) or
+            Carp::croak("$impl does not implement $method");
+    }
+
+    bless { %args }, $class;
+}
+
+sub to_app {
+    my $self = shift;
+    my $app = sub { $self->handle_psgi(@_) };
+    Plack::Middleware::HTTPExceptions->wrap( $app );
+}
+
+sub handle_psgi {
+    my ($self, $env) = @_;
+
+    my $guard = $self->impl->start_request( $env );
+
+    my $req = Plack::Request->new($env);
+    my $res;
+    my $method = $env->{REQUEST_METHOD};
+    if ($method =~ /^(?:GET|HEAD)$/) {
+        $res = $self->get_object( $req );
+    } elsif ($method eq 'PUT') {
+        my $cl = $env->{CONTENT_LENGTH} || 0;
+        if ( $cl == 0 ) {
+            $res = $self->create_bucket( $req );
+        } else {
+            $res = $self->create_object( $req );
+        }
+    } elsif ($method eq 'DELETE') {
+        $res = $self->delete_object( $req );
+    } elsif ($method eq 'POST') {
+        $res = $self->modify_object( $req );
+    } else {
+        $res = $req->new_response(400, [ "Content-Type" => "text/plain" ], [ "Bad Request" ]);
+    }
+
+    return $res->finalize();
+}
+
+sub parse_names {
+    my ($self, $req) = @_;
+    if ( $req->path !~ m{^/([^/]+)(?:/(.+)$)?} ) {
+        if (STF_DEBUG) {
+            warn "Could not parse bucket/object name from " . $req->path;
+        }
+        return ();
+    }
+    return ($1, $2);
+}
+
+sub create_bucket {
+    my ($self, $req) = @_;
+
+    my ($bucket_name, $object_name) = $self->parse_names( $req );
+    if ( $object_name ) {
+        return $req->new_response( 400, [], [ "Bad bucket name $bucket_name/$object_name" ] );
+    }
+
+    my $bucket = $self->impl->get_bucket( {
+        bucket_name => $bucket_name,
+        request     => $req,
+    } );
+    if ($bucket) {
+        return $req->new_response( 204, [], [] );
+    }
+
+    $bucket = $self->impl->create_bucket( {
+        bucket_name => $bucket_name,
+        request     => $req,
+    } );
+    if (! $bucket) {
+        return $req->new_response( 500, [], [ "Failed to create bucket" ] );
+    }
+
+    return $req->new_response( 201, [], [ "Created $bucket_name" ] );
+}
+
+sub create_object {
+    my ($self, $req) = @_;
+
+    # find the appropriate bucket
+    my ($bucket_name, $object_name) = $self->parse_names( $req );
+    my $bucket = $self->impl->get_bucket( {
+        bucket_name => $bucket_name,
+        request     => $req
+    } );
+    if (! $bucket) {
+        return $req->new_response(500, ["Content-Type" => "text/plain"], [ "Failed to find bucket for " . $req->path ] );
+    }
+    if (! $object_name) {
+        return $req->new_response(400, [], ["Could not extract object name"]);
+    }
+
+    # try to find a suffix
+    my ($suffix) = ( $req->path =~ /\.([a-zA-Z0-9]+)$/ );
+    $suffix ||= 'dat';
+
+    my $input = $req->input;
+    $input->seek(0, 0);
+
+    my %ext_args;
+    if ($req->content_type) {
+        $ext_args{content_type} = $req->content_type;
+    }
+
+    my $object = $self->impl->create_object( {
+        bucket      => $bucket,
+        consistency => $req->header( STF_CONSISTENCY_HEADER ) || 0,
+        object_name => $object_name,
+        size        => $req->content_length || 0,
+        suffix      => $suffix,
+        input       => $input,
+        replicas    => $req->header( STF_REPLICATION_HEADER ) ||
+                       $req->header( STF_REPLICATION_HEADER_DEPRECATED ) ||
+                       STF_DEFAULT_REPLICATION_COUNT || 0,
+        request     => $req,
+        %ext_args
+    } );
+    if (! $object) {
+        return $req->new_response( 500, [], [ "Failed to create object" ] );
+    }
+    return $req->new_response( 201, [], [ "Created " . $req->path ] );
+}
+
+sub delete_object {
+    my ($self, $req) = @_;
+
+    # find the appropriate bucket
+    my ($bucket_name, $object_name) = $self->parse_names( $req );
+    my $bucket = $self->impl->get_bucket( {
+        bucket_name => $bucket_name,
+        request     => $req
+    } );
+    if (! $bucket) {
+        if ( ! $object_name ) {
+            return $req->new_response(404, ["Content-Type" => "text/plain"], [ "No such bucket $bucket_name" ] );
+        } else {
+            return $req->new_response(500, ["Content-Type" => "text/plain"], [ "Failed to find bucket for " . $req->path ] );
+        }
+    }
+
+    # if there's no object_name, then this is a request to delete
+    # the bucket, not an object
+    if ( ! $object_name ) {
+        my $bucket = $self->impl->get_bucket( {
+            bucket_name => $bucket_name,
+            request     => $req,
+        } );
+
+        my $ret = $self->impl->delete_bucket( {
+            bucket    => $bucket,
+            recursive => $req->header( STF_RECURSIVE_DELETE_HEADER ) || 0,
+            request   => $req,
+        } );
+        if (! $ret) {
+            return $req->new_response(500, ["Content-Type" => "text/plain"], ["Failed to delete bucket " . $bucket_name]);
+        }
+
+        if ( my $klass = Scalar::Util::blessed($ret) ) {
+            if ($klass->isa('Plack::Response') ) {
+                return $ret;
+            }
+        }
+
+        return $req->new_response( 204, [], [] );
+    }
+
+    my $is_valid = $self->impl->is_valid_object( {
+        bucket      => $bucket,
+        object_name => $object_name,
+        request     => $req,
+    });
+    if (! $is_valid) {
+        return $req->new_response(404, [], [ "No such object " . $req->path ]);
+    }
+
+    if ($self->impl->delete_object( {
+        bucket      => $bucket,
+        object_name => $object_name,
+        request     => $req,
+    } )) {
+        return $req->new_response( 204, [], [] );
+    } else {
+        return $req->new_response( 500, [ "Content-Type" ], [ "Failed to delete " . $req->path ] );
+    }
+}
+
+sub get_object {
+    my ($self, $req) = @_;
+    # find the appropriate bucket
+    my ($bucket_name, $object_name) = $self->parse_names( $req );
+    my $bucket = $self->impl->get_bucket( {
+        bucket_name => $bucket_name,
+        request     => $req
+    } );
+    if (! $bucket) {
+        return $req->new_response(500, ["Content-Type" => "text/plain"], [ "Failed to find bucket for " . $req->path ] );
+    }
+
+    my $object = $self->impl->get_object( {
+        bucket       => $bucket,
+        object_name  => $object_name,
+        request      => $req,
+        force_master => $req->header( STF_FORCE_MASTER_HEADER ) || 0,
+    } );
+    if (! $object) {
+        return $req->new_response( 404, [], [ "Failed to get object " . $req->path ] );
+    }
+    if ( my $klass = Scalar::Util::blessed($object) ) {
+        if ($klass->isa('Plack::Response') ) {
+            return $object;
+        }
+    }
+
+    my @headers;
+    if ( my $ct = $object->can('content_type') ) {
+        push @headers, "Content-Type", $object->content_type;
+    }
+    if ( my $lm = $object->can('modified_on') ) {
+        push @headers, "Last-Modified", HTTP::Date::time2str($object->modified_on);
+    }
+
+    return $req->new_response( 200,
+        \@headers,
+        [ $req->method eq 'HEAD' ? '' : $object->content ]
+    );
+}
+
+sub modify_object {
+    my ($self, $req) = @_;
+
+    my ($bucket_name, $object_name) = $self->parse_names( $req );
+    my $bucket = $self->impl->get_bucket( {
+        bucket_name => $bucket_name,
+        request     => $req
+    } );
+    if (! $bucket) {
+        return $req->new_response(500, ["Content-Type" => "text/plain"], [ "Failed to find bucket for " . $req->path ] );
+    }
+
+    my $ret = $self->impl->modify_object( {
+        bucket      => $bucket,
+        object_name => $object_name,
+        replicas    => $req->header( STF_REPLICATION_HEADER ) ||
+                       $req->header( STF_REPLICATION_HEADER_DEPRECATED ) ||
+                       STF_DEFAULT_REPLICATION_COUNT || 0,
+        request     => $req,
+    } );
+
+    if ( my $klass = Scalar::Util::blessed($ret) ) {
+        if ($klass->isa('Plack::Response') ) {
+            return $ret;
+        }
+    }
+
+    return $req->new_response(204, [], []);
+}
+
+1;
+
+__END__
+
+=head1 NAME 
+
+STF::Dispatcher::PSGI - Pluggable STF Dispatcher Interface
+
+=head1 SYNOPSIS
+
+    # in your stf.psgi
+    use STF::Dispatcher::PSGI;
+
+    my $object = ...;
+    STF::Dispatcher::PSGI->new( impl => $object )->psgi_app;
+
+=head1 DESCRIPTION
+
+STF::Dispatcher::PSGI implements the basic STF Protocol (http://stf-storage.github.com) dispatcher component. It does not know how to actually store or retrieve data, so you must implement that portion yourself. 
+
+The reason this exists is mainly to allow you to testing systems that interact with STF servers. For example, setting up the main L<STF|STF implementation> is quite a pain if all you want to do is to test your application, but with this module, you can easily create a dummy STF dispatcher.
+
+For example, you can use STF::Dispatcher::Impl::Memory (which stores all data in a has in memory) for your tests:
+
+    # in your stf.psgi
+    use STF::Dispatcher::PSGI;
+    use STF::Dispatcher::Impl::Hash;
+
+    my $object = STF::Dispatcher::Impl::Hash->new();
+    STF::Dispatcher::PSGI->new( impl => $object )->psgi_app;
+
+And then you can do something like below in your application test to start a dummy STF server with Plack:
+
+    use Plack::Runner;
+    use Test::TCP;
+
+    my $guard = Test::TCP->new(sub {
+        my $port = shift;
+        my $runner = Plack::Runner->new;
+        $runner->parse_options('-p' => $port);
+        $runner->run( do "stf.psgi" );
+    });
+
+    my $stf_uri = sprintf "http://127.0.0.1:%d", $guard->port;
+    $ua->get( "$stf_uri/path/to/myobject.png" );
+
+Of course, this is not only useful for testing, but it allows you to create a STF clone with a completely different backend without having to reimplement the entire STF protocol.
+
+=head1 THE "IMPLEMENTATION" OBJECT
+
+As described elsewhere, this module by itself DOES NOT work as a real STF server. This module will parse the request and extract the required data from that request, but has no idea how to actually use it. You must therefore provide it with an "implementation".
+
+The simplest implementation is provided with this distribution: STF::Dispatcher::Impl::Memory. This implementation simply puts all the objects in an in-memory hash. See L<STF|STF> for a heavy duty example.
+
+You can choose to create your own STF implementation. In that case, you need to implement list of methods described later.
+
+In these methods, you may choose to throw an exception instead of returning a response. For example, in L<STF|STF>, we use X-Reproxy-URL to serve the objects. This means we cannot just return the fetched object. In that case, we throw an exception that L<Plack::Middleware::HTTPExceptions> can handle (our to_app method automatically enables Plack::Middleware::HTTPExceptions).
+
+See the documentation for that module for details.
+
+=head1 LIST OF REQUIRED METHODS IN THE IMPLEMENTATION
+
+=head2 $object = $impl->E<gt>create_bucket(%args)
+
+Used to create a bucket.
+
+The implementation's get_bucket method will receive the following named parameters:
+
+=over 4
+
+=item B<request> => $object
+
+Plack::Request for this request
+
+=item B<bucket_name> => $string
+
+The name of the bucket
+
+=back
+
+=head2 $object = $impl->E<gt>get_bucket(%args)
+
+Used to retrieve a bucket. If there are no buckets that match the request, you should return undef.
+
+The implementation's get_bucket method will receive the following named parameters:
+
+=over 4
+
+=item B<request> => $object
+
+Plack::Request for this request
+
+=item B<bucket_name> => $string
+
+The name of the bucket
+
+=back
+
+=head2 $object = $impl->E<gt>get_object(%args)
+
+Used to retrieve an object. If there are no object that matcht the request, you should return undef.
+
+Note that this method will be called for both GET and HEAD requests.
+
+The implementation's get_object method will receive the following named parameters:
+
+=over 4
+
+=item B<request> => $object
+
+Plack::Request for this request
+
+=item B<bucket> => $object
+
+The bucket returned by get_bucket().
+
+=item B<object_name> => $string
+
+The name of the object.
+
+=item B<force_master> => $bool
+
+Set to true if X-STF-Force-MasterDB header was sent
+
+=back
+
+=head2 $impl-E<gt>delete_bucket(%args)
+
+=over 4
+
+=item B<request> => $object
+
+Plack::Request for this request
+
+=item B<bucket> => $object
+
+The bucket returned by get_bucket().
+
+=item B<recursive> => $bool
+
+Set to true if the X-STF-Recursive-Delete header was specified
+
+=back
+
+=head2 $impl->E<gt>create_object(%args)
+
+=over 4
+
+=item B<request> => $object
+
+Plack::Request for this request
+
+=item B<bucket> => $object
+
+The bucket returned by get_bucket().
+
+=item B<object_name> => $string
+
+The name of the object.
+
+=item B<consistency> => $int
+
+The minimum consistency (number of replicas that must be created by the end of create_object call.
+
+=item B<size> => $int
+
+The size of the object
+
+=item B<suffix> => $string
+
+The suffix to be used for the object. defaults to ".dat"
+
+=item B<input> => $handle
+
+The input handle to read the data from
+
+=item B<replicas> => $int
+
+Number of replicas that the system should keep in the end.
+
+=back
+
+=head2 $impl-E<gt>modify_object(%args)
+
+=over 4
+
+=item B<replicas> => $int
+
+Number of replicas that the system should keep in the end.
+
+=item B<request> => $object
+
+Plack::Request for this request
+
+=item B<bucket> => $object
+
+The bucket returned by get_bucket().
+
+=item B<object_name> => $string
+
+The name of the object.
+
+=back
+
+=head2 $impl-E<gt>delete_object(%args)
+
+=over 4
+
+=item B<request> => $object
+
+Plack::Request for this request
+
+=item B<bucket> => $object
+
+The bucket returned by get_bucket().
+
+=item B<object_name> => $string
+
+The name of the object.
+
+=back
+
+=head1 AUTHOR
+
+Daisuke Maki C<< <daisuke@endeworks.jp> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by Daisuke Maki
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
---
+++ lib/STF/Dispatcher/Test.pm
@@ -0,0 +1,210 @@
+package STF::Dispatcher::Test;
+use strict;
+use base qw(Exporter);
+use Plack::Test ();
+use HTTP::Date ();
+use HTTP::Request::Common qw(PUT HEAD GET DELETE POST);
+use Test::More;
+
+our @EXPORT = qw( test_stf_impl );
+
+sub test_stf_impl {
+    my $impl = shift;
+    Plack::Test::test_psgi(
+        app => STF::Dispatcher::PSGI->new( impl => $impl )->to_app,
+        client => \&run_tests,
+    );
+}
+
+sub run_tests {
+    my $cb = shift;
+
+    my $res;
+    my $randstr = sub {
+        my $len = shift;
+        my @chars = ('a'..'z', 0..9);
+        return join '', map { $chars[rand @chars] } 1..$len;
+    };
+    my $bucket_name = $randstr->(8);
+    my $object_name = join "/", map { $randstr->(8) } 1..4;
+
+    $res = $cb->(
+        PUT "http://127.0.0.1/$bucket_name/should_fail"
+    );
+    if (! ok ! $res->is_success, "bucket creation request with object_name should fail") {
+        diag $res->as_string;
+    }
+
+    $res = $cb->(
+        PUT "http://127.0.0.1/$bucket_name"
+    );
+    if (! ok $res->is_success, "bucket creation request was successful") {
+        diag $res->as_string;
+    }
+
+    # Creating a bucket again should respond with a 204
+    $res = $cb->(
+        PUT "http://127.0.0.1/$bucket_name"
+    );
+    if (! ok $res->is_success, "bucket creation request (again) was successful") {
+        diag $res->as_string;
+    }
+    if (! is $res->code, 204, "status should be 204") {
+        diag $res->as_string;
+    }
+
+    my $content = join ".", $$, time(), {}, rand();
+
+    # Creating an object with non-existent bucket should fail
+    $res = $cb->(
+        PUT "http://127.0.0.1/$bucket_name-non-existent/$object_name",
+            "X-Replication-Count" => 3,
+            "Content-Type" => "text/plain",
+            Content => $content,
+    );
+    if (! ok ! $res->is_success, "object creation request should fail") {
+        diag $res->as_string;
+    }
+
+    # Creating an object without giving an object name should fail
+    $res = $cb->(
+        PUT "http://127.0.0.1/$bucket_name",
+            "X-Replication-Count" => 3,
+            "Content-Type" => "text/plain",
+            Content => $content,
+    );
+    if (! ok ! $res->is_success, "object creation request should fail") {
+        diag $res->as_string;
+    }
+
+    # normal object creation
+    $res = $cb->(
+        PUT "http://127.0.0.1/$bucket_name/$object_name",
+            "X-Replication-Count" => 3,
+            "Content-Type" => "text/plain",
+            Content => $content,
+    );
+    if (! ok $res->is_success, "object creation request was successful") {
+        diag $res->as_string;
+    }
+
+    # Get an object with a bad object name
+    $res = $cb->(
+        GET "http://127.0.0.1/$bucket_name/$object_name-non-existent",
+    );
+    if (! ok ! $res->is_success, "object fetch request should fail") {
+        diag $res->as_string;
+    }
+
+    $res = $cb->(
+        GET "http://127.0.0.1/$bucket_name/$object_name",
+    );
+    is $res->content, $content, "content matches";
+
+    if ( my $last_mod = $res->header('Last-Modified') ) {
+        my $last_mod_t = HTTP::Date::str2time($last_mod);
+        $res = $cb->(
+            GET "http://127.0.0.1/$bucket_name/$object_name",
+                "If-Modified-Since" => $last_mod
+        );
+        if ( ! is $res->code, 200, "code is 200") {
+            diag $res->as_string;
+        }
+
+        $res = $cb->(
+            GET "http://127.0.0.1/$bucket_name/$object_name",
+                "If-Modified-Since" => HTTP::Date::time2str($last_mod_t - 30)
+        );
+        if (! is $res->code, 304, "code is 304!") {
+            diag $res->as_string;
+        }
+    }
+
+
+    TODO: {
+        # Content-Type handling should probably be removed from
+        # STF::Dispatcher
+        todo_skip "This is probably not necessary", 1;
+        is $res->content_type, "text/plain", "content type matches";
+    }
+
+    $res = $cb->(
+        HEAD "http://127.0.0.1/$bucket_name/$object_name",
+    );
+    ok $res->is_success, "HEAD works";
+    ok ! $res->content, "Should have no body";
+
+    # Modify the object
+    $res = $cb->(
+        POST "http://127.0.0.1/$bucket_name/$object_name",
+    );
+    ok $res->is_success, "POST works";
+
+
+    # Deleting an object with a bad bucket name
+    $res = $cb->(
+        DELETE "http://127.0.0.1/$bucket_name-non-existent/$object_name"
+    );
+    if (! ok ! $res->is_success, "object deletion request should fail") {
+        diag $res->as_string;
+    }
+
+    # Deleting an object with a bad object name
+    $res = $cb->(
+        DELETE "http://127.0.0.1/$bucket_name/$object_name-non-existent"
+    );
+    if (! ok ! $res->is_success, "object deletion request should fail") {
+        diag $res->as_string;
+    }
+
+    # Normal object deletion
+    $res = $cb->(
+        DELETE "http://127.0.0.1/$bucket_name/$object_name"
+    );
+    if (! ok $res->is_success, "object deletion request was successful") {
+        diag $res->as_string;
+    }
+
+    # now that the object is deleted, this should fail
+    $res = $cb->(
+        GET "http://127.0.0.1/$bucket_name/$object_name",
+    );
+    if (! is $res->code, 404, "get after delete is 404" ) {
+        diag $res->as_string;
+    }
+
+    # non-existent bucket deletion should fail
+    $res = $cb->(
+        DELETE "http://127.0.0.1/$bucket_name-non-existent"
+    );
+    if (! is $res->code, 404, "bucket deletion request was 404") {
+        diag $res->as_string;
+    }
+
+    # normal bucket deletion
+    $res = $cb->(
+        DELETE "http://127.0.0.1/$bucket_name"
+    );
+    if (! is $res->code, 204, "bucket deletion request was 204") {
+        diag $res->as_string;
+    }
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+STF::Dispatcher::Test - Basic Tests For STF Implementations
+
+=head1 SYNOPSIS
+
+    use Test::More;
+    use STF::Dispatcher::Test;
+
+    test_stf_impl My::STF::Impl->new;
+
+    done_testing;
+
+=cut
