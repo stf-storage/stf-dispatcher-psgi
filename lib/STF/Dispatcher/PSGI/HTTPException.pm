035ce221b79e99d7d223d21e77bc6b455ef5afbe
lestrrat <lestrrat+github@gmail.com>
2011-11-30 01:51:29 +0900

    add docs

--- lib/STF/Dispatcher/PSGI/HTTPException.pm
+++ lib/STF/Dispatcher/PSGI/HTTPException.pm
@@ -12,3 +12,27 @@ sub as_psgi {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+STF::Dispatcher::PSGI::HTTPException - Very Light Exception For STF Dispatcher
+
+=head1 SYNOPSIS
+
+    use STF::Dispatcher::PSGI::HTTPException;
+    STF::Dispatcher::PSGI::HTTPException->throw( $code );
+    STF::Dispatcher::PSGI::HTTPException->throw( $code, \@hdrs );
+    STF::Dispatcher::PSGI::HTTPException->throw( $code, \@hdrs, \@content );
+
+=head1 DESCRIPTION
+
+This class is a very lightweight fallback for STF Dispatcher modules to abort
+request processing and immediately return a HTTP response.
+
+This is meant to be used in conjunction with Plack::Middleware::HTTPExceptions. 
+
+You are free to use other modules such ash HTTP::Exception. This module only exists so that we don't have to add an extra dependency to STF::Dispatcher::PSGI (which should just be the interface, so should be as light as possible)
+
+=cut
